a9fab52b47486f0357dd2d9255b7fdddc70f845b
violent-capybara <98543271+violent-capybara@users.noreply.github.com>
2022-04-29 00:37:37 +0530

    Change wrong query for data

--- app/Http/Controllers/PdfControlller.php
+++ app/Http/Controllers/PdfControlller.php
@@ -62,30 +62,24 @@ class PdfControlller extends Controller
     public function showExceldata(){
         
 
-        $excelData=DB::table('block_muni')
-                      ->select('block_muni.blockmuni', 
-                                'kishan_credit_card.KCC_target', 
-                                'kishan_credit_card.KCC_sponsored', 
-                                'kishan_credit_card.KCC_sanctioned', 
-                                'kishan_credit_card.Percentage_sponsored',
-                                'block_muni.blockmuni', 
-                                'kishan_credit_card.KCC_target', 
-                                'kishan_credit_card.KCC_sponsored', 
-                                'kishan_credit_card.KCC_sanctioned', 
-                                'kishan_credit_card.Percentage_sponsored',
-                                'kishan_mandi.KM_operational',
-                                'mgnregs.tot_person_days_generate', 
-                                'mgnregs.KCC_sponsored', 
-                                'mgnregs.avg_persondays_per_household', 
-                                'mgnregs.percentage_of_labour_budget_achieved',
-                                'mgnregs.expenditure_made_under_mgnrega',
-                                'anandadhara.tot_SHGs_formed', 
-                                'anandadhara.tot_SHGs_credit_linkage')
-                      ->leftJoin('kishan_credit_card','block_muni.blockminicd','=','kishan_credit_card.blockminicd')
-                      ->leftJoin('kishan_mandi','block_muni.blockminicd','=','kishan_mandi.blockminicd')
-                      ->leftJoin('mgnregs','block_muni.blockminicd','=','mgnregs.blockminicd')
-                      ->leftJoin('anandadhara','block_muni.blockminicd','=','anandadhara.blockminicd')
-                      ->get();
+       $excelData=DB::table('block_muni')
+                        ->select('block_muni.blockmuni', 
+                        'kishan_credit_card.KCC_target', 
+                        'kishan_credit_card.KCC_sponsored', 
+                        'kishan_credit_card.KCC_sanctioned', 
+                        'kishan_credit_card.Percentage_sponsored',
+                        'kishan_mandi.KM_operational',
+                        'mgnregs.tot_person_days_generate', 
+                        'mgnregs.avg_persondays_per_household', 
+                        'mgnregs.expenditure_made_under_mgnrega',
+                        'mgnregs.percentage_of_labour_budget_achieved',
+                        'anandadhara.tot_SHGs_formed', 
+                        'anandadhara.tot_SHGs_credit_linkage')
+                        ->leftJoin('kishan_credit_card','block_muni.blockminicd','=','kishan_credit_card.blockminicd')
+                ->leftJoin('kishan_mandi','block_muni.blockminicd','=','kishan_mandi.blockminicd')
+                ->leftJoin('mgnregs','block_muni.blockminicd','=','mgnregs.blockminicd')
+                ->leftJoin('anandadhara','block_muni.blockminicd','=','anandadhara.blockminicd')
+                ->get();
         // dd($excelData);              
         // $kishanCreditCard=DB::table('block_muni')
         //                     ->select('block_muni.blockmuni', 'kishan_credit_card.KCC_target', 'kishan_credit_card.KCC_sponsored', 'kishan_credit_card.KCC_sanctioned', 'kishan_credit_card.Percentage_sponsored')
