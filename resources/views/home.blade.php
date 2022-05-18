ef00d9e62704c22b91dfe9d9552387461f47a333
Imran Khan <imrankhan96799@gmail.com>
2022-05-19 04:47:31 +0530

    Change footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,18 +8,21 @@
         }
 
         .footer_item_size {
-            height: 50px;
+            height: 60px;
         }
-        .page-footer{
-            height: 50px;
-        }
+      
        
-       .sign-in{
-            margin-bottom: 30px !important;
-       }
+   
        .carousel-image-size{
         width: 100%;
         height: 750px;
+       }
+       .footer{
+         margin-top:7px;
+         
+       }
+       #sign_in{
+         margin-top: 8px;
        }
 
     </style>
@@ -29,112 +32,31 @@
 
   @include('common_carousel')
  
-
-
+  
+  <div class="footer">
+    <div class="container">
     
-<footer class="page-footer font-small mdb-color lighten-3 pt-4  " >
-
-    <!-- Footer Elements -->
-    <div class="container" >
-  
-      <!--Grid row-->
-      <div class="row" >
-  
-        <!--Grid column-->
-        <div class="col-lg-2 col-md-12 mb-4"  >
-  
-          <!--Image-->
-          <div class="view overlay z-depth-1-half" >
-            <img  src="{{ URL('assets/logos/kcc.png') }}" class="img-fluid footer_item_size" />
-              <div class="mask rgba-white-light"></div>
-            
-          </div>
-  
+      <div class="row">
+        <div class="col">
+          <img class="footer_item_size" alt="" src="{{ URL('assets/logos/kcc.png') }}" />
         </div>
-        <!--Grid column-->
-  
-        <!--Grid column-->
-        <div class="col-lg-2 col-md-6 mb-4">
-  
-          <!--Image-->
-          <div class="view overlay z-depth-1-half ">
-            <img  src="{{ URL('assets/logos/kisan_mandi.png') }}" class="img-fluid footer_item_size" />
-              <div class="mask rgba-white-light"></div>
-          
-          </div>
-  
+        <div class="col">
+          <img class="footer_item_size" alt="" src="{{ URL('assets/logos/kisan_mandi.png') }}" />
         </div>
-        <!--Grid column-->
-        <div class="col-lg-2 col-md-12 mb-4" style="padding-top: 26px" >
-  
-       
-            <div   class="view overlay z-depth-1-hal " >
-             <a  class=" btn btn-primary sign_in" href="{{ route('index') }}">Sign in</a>
-                <div class="mask rgba-white-light"></div>
-              
-            </div>
-    
-          </div>
-        <!--Grid column-->
-        <div class="col-lg-2 col-md-6 mb-4">
-  
-          <!--Image-->
-          <div class="view overlay z-depth-1-half ">
-            <img  src="{{ URL('assets/logos/anandadhara.jpg') }}" class="img-fluid footer_item_size" />
-              <div class="mask rgba-white-light"></div>
-           
-          </div>
-  
+        <div class="col">
+          <a id="sign_in"  href="{{ route('index') }}" class="btn btn-primary">Sign in</a>
         </div>
-        <!--Grid column-->
-  
-        <!--Grid column-->
-        <div class="col-lg-2 col-md-12 mb-4">
-  
-          <!--Image-->
-          <div class="view overlay z-depth-1-half ">
-            <img  src="{{ URL('assets/logos/mgnrega.png') }}" class="img-fluid footer_item_size " />
-              <div class="mask rgba-white-light"></div>
-           
-          </div>
-  
+        <div class="col">
+          <img class="footer_item_size" alt="" src="{{ URL('assets/logos/anandadhara.jpg') }}" />
         </div>
-        <!--Grid column-->
-  
-        <!--Grid column-->
-        {{-- <div class="col-lg-2 col-md-6 mb-4">
-  
-          <!--Image-->
-          <div class="view overlay z-depth-1-half">
-            <img  src="{{ URL('assets/logos/kcc.png') }}" class="img-fluid footer_image_size" />
-              <div class="mask rgba-white-light"></div>
-           
-          </div>
-  
-        </div> --}}
-        <!--Grid column-->
-  
-        <!--Grid column-->
-        {{-- <div class="col-lg-2 col-md-6 mb-4">
-  
-          <!--Image-->
-          <div class="view overlay z-depth-1-half">
-            <img  src="{{ URL('assets/logos/kcc.png') }}" class="img-fluid footer_image_size" />
-              <div class="mask rgba-white-light"></div>
-            
-          </div>
-  
-        </div> --}}
-        <!--Grid column-->
-  
+        <div class="col">
+          <img class="footer_item_size" alt="" src="{{ URL('assets/logos/mgnrega.png') }}" />
+        </div>
       </div>
-      <!--Grid row-->
-  
-    </div>
-    <!-- Footer Elements -->
-  
-  
-  </footer>
+    
+  </div>
+  </div>
+    
 
 
 @endsection
